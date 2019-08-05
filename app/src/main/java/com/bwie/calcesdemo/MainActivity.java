180c70beabffc1bf6c5b5af2ab907f4f793d48fc
wangjiahu <1219477246@qq.com>
2019-08-05 16:57:38 +0800

    新加toast

--- app/src/main/java/com/bwie/calcesdemo/MainActivity.java
+++ app/src/main/java/com/bwie/calcesdemo/MainActivity.java
@@ -4,6 +4,7 @@ package com.bwie.calcesdemo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("hello", "hello");
+        Toast.makeText(, "", Toast.LENGTH_SHORT).show();
     }
 }
